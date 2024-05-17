fd66e459a74c39c0f0e46277b46005d960715931
emiliofigueroa1 <157066769+emiliofigueroa1@users.noreply.github.com>
2024-05-16 23:33:29 -0400

    Create LatLong.java

---
+++ LatLong.java
@@ -0,0 +1,17 @@
+public class LatLong {
+    private String latitude;
+    private String longitude;
+
+    public LatLong(String lat, String lon) {
+        latitude = lat;
+        longitude = lon;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+}
